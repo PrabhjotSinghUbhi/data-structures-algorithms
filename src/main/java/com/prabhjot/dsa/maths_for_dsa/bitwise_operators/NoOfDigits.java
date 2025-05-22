0f2fa6f33f20e02fd7d80cfb584b9a703b08586a
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-05-22 23:51:42 +0530

    Count the number of bits made into a method calc.

--- src/main/java/com/prabhjot/dsa/maths_for_dsa/bitwise_operators/NoOfDigits.java
+++ src/main/java/com/prabhjot/dsa/maths_for_dsa/bitwise_operators/NoOfDigits.java
@@ -3,8 +3,12 @@ package com.prabhjot.dsa.maths_for_dsa.bitwise_operators;
 public class NoOfDigits {
     public static void main(String[] args) {
         // Your code here
-        int num = 64;
-        int digits = (int) (Math.log(num) / Math.log(2)) + 1;
+        int num = 6449858;
+        int digits = calc(num,10);
         System.out.println(digits);
+    }
+
+    public static int calc(int number, int base){
+        return (int) (Math.log(number) / Math.log(base)) + 1;
     }
 }
