ec802d5137c04a9a129d14b6d9a368b02813a87f
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-05-28 23:34:00 +0530

    Learned Sieve of Eratosthenes.

---
+++ src/main/java/com/prabhjot/dsa/maths_for_dsa/prime_numbers/sieve_of_eratosthenes/SieveOfEratosthenes.java
@@ -0,0 +1,26 @@
+package com.prabhjot.dsa.maths_for_dsa.prime_numbers.sieve_of_eratosthenes;
+
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        int num = 37;
+        boolean[] primes = new boolean[num + 1];
+        sieveOfEratosthenes(num, primes);
+    }
+
+    public static void sieveOfEratosthenes(int num, boolean[] primes) {
+        // false means prime.
+        for (int i = 2; i * i <= num; i++) {
+            // if it is prime.
+            if (!primes[i]) {
+                for (int j = i * i; j <= num; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= num; i++) {
+            if (!primes[i])
+                System.out.printf("%d ", i);
+        }
+    }
+}
