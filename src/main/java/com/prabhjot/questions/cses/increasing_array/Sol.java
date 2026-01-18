928ea62503de4a3c79dc9555489987d7717399c8
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-01-19 00:10:42 +0530

    done cses questions

---
+++ src/main/java/com/prabhjot/questions/cses/increasing_array/Sol.java
@@ -0,0 +1,8 @@
+package com.prabhjot.questions.cses.increasing_array;
+import java.util.*;
+
+public class sol {
+    public static void main(String[] args) {
+        $
+    }
+}
---
+++ src/main/java/com/prabhjot/questions/cses/permutations/Sol.java
@@ -0,0 +1,8 @@
+package com.prabhjot.questions.cses.permutations;
+import java.util.*;
+
+public class Sol {
+    public static void main(String[] args) {
+        $
+    }
+}
