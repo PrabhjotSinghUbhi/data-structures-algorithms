9c89eb3d196c95cbf879a9e314e2a3c03b42f09a
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-01-27 14:33:43 +0530

    started learning jdbc.

---
+++ src/main/java/com/prabhjot/jdbc/introduction/Introduction.java
@@ -0,0 +1,9 @@
+package com.prabhjot.jdbc.introduction;
+
+import java.util.*;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("JDBC begins");
+    }
+}
